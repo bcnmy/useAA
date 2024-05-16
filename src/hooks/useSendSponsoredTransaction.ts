3d8be6e3136e83a5e91233fb2834c9d8a0fa24f7
Joe Pegler <joepegler123@gmail.com>
2024-05-16 12:58:46 +0100

    chore: uppercase type

--- src/hooks/useSendSponsoredTransaction.ts
+++ src/hooks/useSendSponsoredTransaction.ts
@@ -1,49 +1,49 @@
 import {
   BuildUserOpOptions,
   PaymasterMode,
-  Transaction,
-} from "@biconomy/account";
-import { useMutation } from "@tanstack/react-query";
-import { useSmartAccount } from "./useSmartAccount";
-import { MutationOptionsWithoutMutationFn } from "../types/mutation";
+  Transaction
+} from "@biconomy/account"
+import { useMutation } from "@tanstack/react-query"
+import { useSmartAccount } from "./useSmartAccount"
+import { MutationOptionsWithoutMutationFn } from "../types/mutation"
 
 type BuildUserOpOptionsWithoutPaymasterServiceData = Omit<
   BuildUserOpOptions,
   "paymasterServiceData"
->;
+>
 
-type useSendSponsoredTransactionArgs = {
-  manyOrOneTransactions: Transaction | Transaction[];
-  buildUseropDto?: BuildUserOpOptionsWithoutPaymasterServiceData;
-};
+type UseSendSponsoredTransactionArgs = {
+  manyOrOneTransactions: Transaction | Transaction[]
+  buildUseropDto?: BuildUserOpOptionsWithoutPaymasterServiceData
+}
 
 export const useSendSponsoredTransaction = (
   mutationArgs?: MutationOptionsWithoutMutationFn
 ) => {
-  const { smartAccountClient, queryClient } = useSmartAccount();
+  const { smartAccountClient, queryClient } = useSmartAccount()
 
   const useSendSponsoredTransactionMutation = useMutation(
     {
-      mutationFn: async (params: useSendSponsoredTransactionArgs) => {
+      mutationFn: async (params: UseSendSponsoredTransactionArgs) => {
         if (!smartAccountClient) {
-          throw new Error("No smart account found!");
+          throw new Error("No smart account found!")
         }
 
-        const { manyOrOneTransactions, buildUseropDto } = params;
+        const { manyOrOneTransactions, buildUseropDto } = params
 
         const result = await smartAccountClient.sendTransaction(
           manyOrOneTransactions,
           {
             ...buildUseropDto,
-            paymasterServiceData: { mode: PaymasterMode.SPONSORED },
+            paymasterServiceData: { mode: PaymasterMode.SPONSORED }
           }
-        );
-        return result;
+        )
+        return result
       },
-      ...mutationArgs,
+      ...mutationArgs
     },
     queryClient
-  );
+  )
 
-  return useSendSponsoredTransactionMutation;
-};
+  return useSendSponsoredTransactionMutation
+}
