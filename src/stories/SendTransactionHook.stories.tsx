e399791fac27f77c8b542c7dba754d0cf6214090
gunnmgirl <gunnmgirl@gmail.com>
2024-05-20 23:00:05 +0200

    feat: adds storybook control wait for tx hash

--- src/stories/SendTransactionHook.stories.tsx
+++ src/stories/SendTransactionHook.stories.tsx
@@ -3,20 +3,29 @@ import {
   SendTransaction,
 } from "@/stories/components/SendTransaction";
 import { DeploySmartAccount } from "@/stories/components/DeploySmartAccount";
+import { HookArgs } from "@/stories/utils/types";
 
 export default {
   title: "React Hooks",
   component: SendTransaction,
 };
 
-export const SendTransactionHook = () => {
-  return <SendTransaction />;
+export const SendTransactionHook = (args: HookArgs) => {
+  return <SendTransaction {...args} />;
+};
+
+SendTransactionHook.args = {
+  wait: false,
 };
 
 export const SendSponsoredTransactionHook = () => {
   return <SendSponsoredTransaction />;
 };
 
-export const DeploySmartAccountHook = () => {
-  return <DeploySmartAccount />;
+export const DeploySmartAccountHook = (args: HookArgs) => {
+  return <DeploySmartAccount {...args} />;
+};
+
+DeploySmartAccountHook.args = {
+  wait: false,
 };
---
+++ src/stories/utils/types.ts
@@ -0,0 +1,3 @@
+export type HookArgs = {
+  wait: boolean;
+};
