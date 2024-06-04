8b647b12bf5c76b4a56b15572df8b2fa8f380105
Joe Pegler <joepegler123@gmail.com>
2024-06-04 17:45:12 +0100

    chore: add build scripts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * from "@/hooks";
+export * from "@/utils";
 
 export { BiconomyProvider } from "@/providers";
--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,6 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  test: {
-    globals: true,
-    environment: "happy-dom",
-  },
   resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
   },
